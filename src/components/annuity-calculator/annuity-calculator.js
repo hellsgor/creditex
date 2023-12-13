954bb103c5def06db91cbafd34a72fe7a1b3c598
Igor Storchevoy <hellsgor@gmail.com>
2023-12-13 12:16:12 +0400

    add renderGraph()

--- src/components/annuity-calculator/annuity-calculator.js
+++ src/components/annuity-calculator/annuity-calculator.js
@@ -1,19 +1,26 @@
 import { preparePaymentControlValue } from 'Components/annuity-calculator/_prepare-payment-control-value';
 import { handleInputChange } from 'Components/annuity-calculator/_handle-input-change';
 import { CONSTANTS } from 'Constants/constants';
+import { renderGraph } from 'Components/annuity-calculator/_renderGraph';
 
+const graphBlock = document.getElementById(CONSTANTS.GRAPH_BLOCK_ID);
 const controls = {
-  amountControl: document.getElementById(CONSTANTS.rangeControls.amountNameID),
-  paymentControl: document.getElementById(
-    CONSTANTS.rangeControls.paymentNameID,
+  amountControl: document.getElementById(
+    CONSTANTS.rangeControls.AMOUNT_NAME_ID,
   ),
-  termControl: document.getElementById(CONSTANTS.rangeControls.termNameID),
+  paymentControl: document.getElementById(
+    CONSTANTS.rangeControls.PAYMENT_NAME_ID,
+  ),
+  termControl: document.getElementById(CONSTANTS.rangeControls.TERM_NAME_ID),
 };
 
-preparePaymentControlValue(
-  controls.amountControl,
-  controls.paymentControl,
-  controls.termControl,
+renderGraph(
+  graphBlock,
+  preparePaymentControlValue(
+    controls.amountControl,
+    controls.paymentControl,
+    controls.termControl,
+  ),
 );
 
 [controls.amountControl, controls.paymentControl, controls.termControl].forEach(
@@ -21,6 +28,10 @@ preparePaymentControlValue(
     if (control) {
       control.addEventListener('input', (event) => {
         handleInputChange(event, controls);
+      });
+
+      control.addEventListener('change', (event) => {
+        renderGraph(graphBlock, handleInputChange(event, controls));
       });
     }
   },
