e5bc5e50e5e2b64c5fb1717a7785f5a381acd8af
Igor Storchevoy <hellsgor@gmail.com>
2023-12-17 21:55:45 +0400

    product-line is ready

--- src/components/block-with-tabs/_switchingTabs.js
+++ src/components/block-with-tabs/_switchingTabs.js
@@ -4,24 +4,16 @@ export function switchingTabs(tabsBlock) {
   const tabsInner = tabsBlock.querySelector('.block-with-tabs__tabs-inner');
   const content = tabsBlock.querySelectorAll('.tab__content');
 
-  console.log('tabsBlock', tabsBlock);
-  console.log('tabs', tabs);
-  console.log('content', content);
-
   function tabSwitching(tabsItems, tabsContainer, contentBlocks, selector) {
     function hideTabContent(a) {
-      console.log('a', a);
       for (let i = a; i < contentBlocks.length; i++) {
-        console.log('i', i);
-        contentBlocks[i].classList.remove('tab__content_show');
-        contentBlocks[i].classList.add('tab__content_hide');
         tabs[i].classList.remove('tab_active', 'tab_hidden-after');
+        contentBlocks[i].classList.add('tab__content_hide');
       }
     }
 
     function showTabContent(b) {
       if (contentBlocks[b].classList.contains('tab__content_hide')) {
-        contentBlocks[b].classList.add('tab__content_show');
         contentBlocks[b].classList.remove('tab__content_hide');
         tabs[b].classList.add('tab_active');
         if (b > 0) {
@@ -33,8 +25,6 @@ export function switchingTabs(tabsBlock) {
     hideTabContent(1);
     tabsContainer.addEventListener('click', (event) => {
       const { target } = event;
-
-      console.log(target);
 
       if (target && target.classList.contains(selector)) {
         for (let i = 0; i < tabsItems.length; i++) {
