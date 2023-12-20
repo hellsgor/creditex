12fc4cf0c6deb140e0d8b719168fc5442dd66007
Igor Storchevoy <hellsgor@gmail.com>
2023-12-20 19:43:06 +0400

    fix offer phone mask

--- src/components/offer/offer.js
+++ src/components/offer/offer.js
@@ -1,5 +1,3 @@
 import { addPhoneMask } from 'Utils/phone-mask/phone-mask';
 
-document.querySelectorAll('input[type="tel"]').forEach((telControl) => {
-  addPhoneMask(telControl);
-});
+addPhoneMask();
