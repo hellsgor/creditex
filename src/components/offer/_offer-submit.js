de2886ce18ac658b08101a57e3e5eb1569aeb3e1
Igor Storchevoy <hellsgor@gmail.com>
2023-12-20 22:12:38 +0400

    sending control value to the registration page

--- src/components/offer/_offer-submit.js
+++ src/components/offer/_offer-submit.js
@@ -1,10 +1,16 @@
 import { phoneValidation } from 'Utils/validation/phoneValidation';
 import { showError } from 'Utils/errors/show-error';
+import { sendData } from 'Utils/handle-form-submit/_send-data';
+import { serializeData } from 'Utils/handle-form-submit/_serialize-data';
 
 export function offerSubmit(offerControl) {
   if (offerControl.value) {
     if (phoneValidation(offerControl)) {
-      console.log('ok');
+      // TODO: актуализировать URL в sendData при передаче в back
+      sendData(serializeData([offerControl]), './', false);
+      window.location.href = `./registration.html?phoneNumber=${encodeURIComponent(
+        offerControl.value.toString().replace(/[^\d]/g, ''),
+      )}`;
     } else {
       showError('CE001', offerControl);
     }
