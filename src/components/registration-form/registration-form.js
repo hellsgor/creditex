aeb5fc2465099b34c7c08df4e181c15eb22876ef
Igor Storchevoy <hellsgor@gmail.com>
2023-12-25 19:03:53 +0400

    implemented basic registration controls validation

--- src/components/registration-form/registration-form.js
+++ src/components/registration-form/registration-form.js
@@ -3,6 +3,8 @@ import { REGISTRATION_PAGE_CONSTANTS } from 'Constants/constants';
 import 'UIKit/form-password/form-password';
 import { numbersOnly } from 'Utils/masks/numbers-only-mask';
 import { addPhoneMask } from 'Utils/masks/phone-mask';
+import { registrationValidation } from 'Components/registration-form/_registration-validation';
+import { resetError } from 'Utils/errors/reset-error';
 
 const registrationControls = Object.values(
   REGISTRATION_PAGE_CONSTANTS.registrationFormControls,
@@ -10,10 +12,26 @@ const registrationControls = Object.values(
 
 addPhoneMask();
 
+registrationControls.forEach((control) => {
+  if (
+    control.id
+    !== REGISTRATION_PAGE_CONSTANTS.registrationFormControls.rememberUserCheckbox
+  ) {
+    control.addEventListener('input', (event) => {
+      if (
+        control.id
+        === REGISTRATION_PAGE_CONSTANTS.registrationFormControls.innControl
+      ) {
+        numbersOnly(event.target);
+      }
+      resetError(control);
+    });
+  }
+});
+
 document
-  .getElementById(
-    REGISTRATION_PAGE_CONSTANTS.registrationFormControls.innControl,
-  )
-  .addEventListener('input', (event) => {
-    numbersOnly(event.target);
+  .getElementById(REGISTRATION_PAGE_CONSTANTS.comeInButton)
+  .addEventListener('click', () => {
+    const isValid = registrationValidation(registrationControls);
+    console.log(isValid);
   });
