5c012019e5046aafd74c327f760d713797cd19d7
Igor Storchevoy <hellsgor@gmail.com>
2023-12-26 12:36:38 +0400

    implemented sending confirmation data and response processing

---
+++ src/components/confirmation-form/confirmation-form.js
@@ -0,0 +1,34 @@
+import { CONFIRMATION_PAGE_CONSTANTS } from 'Constants/constants';
+import { numbersOnly } from 'Utils/masks/numbers-only-mask';
+import { resetError } from 'Utils/errors/reset-error';
+import { handleFormSubmit } from 'Utils/handle-form-submit/handle-form-submit';
+import { confirmationResponseHandler } from 'Components/confirmation-form/_confirmation-response-handler';
+import { showError } from 'Utils/errors/show-error';
+import { ERRORS } from 'Constants/errors';
+
+const codeControl = document.getElementById(
+  CONFIRMATION_PAGE_CONSTANTS.confirmationFormControls.confirmationCodeControl,
+);
+codeControl.addEventListener('input', () => {
+  resetError(codeControl);
+  numbersOnly(codeControl);
+});
+
+document
+  .getElementById(CONFIRMATION_PAGE_CONSTANTS.confirmationForwardButton)
+  .addEventListener('click', () => {
+    if (codeControl.value.length && codeControl.value.length <= 8) {
+      handleFormSubmit(
+        [codeControl],
+        // TODO: изменить url при передаче в back
+        // './moc/confirmation-response-error.json',
+        './moc/confirmation-response-success.json',
+        confirmationResponseHandler,
+        true,
+      );
+    } else if (!codeControl.value.length) {
+      showError(ERRORS.CE008(), codeControl);
+    } else {
+      showError(ERRORS.CE002(), codeControl);
+    }
+  });
