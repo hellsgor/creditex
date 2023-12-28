92ade7a5120cd4756ef6a1f72fc38916225248d5
Igor Storchevoy <hellsgor@gmail.com>
2023-12-28 14:13:00 +0400

    added form-password component

--- src/components/new-password-form/new-password-form.js
+++ src/components/new-password-form/new-password-form.js
@@ -2,6 +2,7 @@ import { NEW_PASSWORD_PAGE_CONSTANTS } from 'Constants/constants';
 import { numbersOnly } from 'Utils/masks/numbers-only-mask';
 import { resetError } from 'Utils/errors/reset-error';
 import { newPasswordValidation } from 'Components/new-password-form/_new-password-validation';
+import 'UIKit/form-password/form-password';
 
 const controls = Object.values(
   NEW_PASSWORD_PAGE_CONSTANTS.newPasswordFormControls,
@@ -24,8 +25,17 @@ document
   .getElementById(NEW_PASSWORD_PAGE_CONSTANTS.newPasswordChangeButton)
   .addEventListener('click', () => {
     if (newPasswordValidation(controls)) {
-      console.log('valid');
-    } else {
-      console.log('invalid');
+      handleFormSubmit(
+        controls,
+        // TODO: актуализировать url при передаче в back
+        // 'GET',
+        // './moc/new-password-response-error.json',
+
+        'POST',
+        'http://localhost:4000/endpoint',
+
+        newPasswordResponseHandler,
+        true,
+      );
     }
   });
