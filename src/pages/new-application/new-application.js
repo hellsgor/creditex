6bdda0279d7991b4f12579b981d9ccbe659b4a15
Igor Storchevoy <hellsgor@gmail.com>
2024-01-02 15:55:24 +0400

    getFormControl function refactoring

--- src/pages/new-application/new-application.js
+++ src/pages/new-application/new-application.js
@@ -1,5 +1,5 @@
 import { NEW_APPLICATION_PAGE } from 'Constants/constants';
-import { getFormControls } from 'Utils/get-form-controls';
+import { getControls } from 'Utils/get-controls';
 import { numbersOnly } from 'Utils/masks/numbers-only-mask';
 import { addPhoneMask } from 'Utils/masks/phone-mask';
 import { resetError } from 'Utils/errors/reset-error';
@@ -7,7 +7,7 @@ import { step1Validation } from './_step-1-validation';
 
 const form = document.getElementById(NEW_APPLICATION_PAGE.formId);
 
-const controls = getFormControls(form);
+const controls = getControls(form);
 
 controls.forEach((control) => {
   control.addEventListener('input', () => {
@@ -29,9 +29,7 @@ document
   .getElementById(NEW_APPLICATION_PAGE.buttons.goToStep2)
   .addEventListener('click', () => {
     const isStep1Valid = step1Validation(
-      getFormControls(
-        document.getElementById(NEW_APPLICATION_PAGE.sections.step1),
-      ),
+      getControls(document.getElementById(NEW_APPLICATION_PAGE.sections.step1)),
     );
     console.log(isStep1Valid);
   });
