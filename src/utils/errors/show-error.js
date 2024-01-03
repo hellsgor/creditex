b4f882dc781073f5020b20a202d103565f6bc9e0
Igor Storchevoy <hellsgor@gmail.com>
2024-01-03 12:05:29 +0400

    fixed showError hidden controls

--- src/utils/errors/show-error.js
+++ src/utils/errors/show-error.js
@@ -1,4 +1,8 @@
 export function showError(errorFunc, control) {
+  if (control.type === 'checkbox' || control.type === 'hidden') {
+    return;
+  }
+
   const errorElem = control.closest('.form-control')
     ? control.closest('.form-control').querySelector('.form-control__error')
     : control.closest('.form-select').querySelector('.form-select__error');
