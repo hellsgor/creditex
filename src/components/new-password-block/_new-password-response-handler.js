ef76a981308bb0b31f8eb7a83f57d543c56949ab
Igor Storchevoy <hellsgor@gmail.com>
2024-01-15 11:35:19 +0400

    fix - remove new password message block

--- src/components/new-password-block/_new-password-response-handler.js
+++ src/components/new-password-block/_new-password-response-handler.js
@@ -1,7 +1,6 @@
 import { isSuccess } from 'Utils/handle-form-submit/_isSuccess';
 import { errorsProcessingFromResponse } from 'Utils/errors/errors-processing-from-response';
 import { NEW_PASSWORD_PAGE } from 'Constants/constants';
-import { newPasswordShowMessage } from 'Components/new-password-block/_new-password-show-message';
 
 export function newPasswordResponseHandler(
   response,
@@ -14,9 +13,7 @@ export function newPasswordResponseHandler(
       controlsArrayOrForm,
       defaultErrorControl,
     );
-    newPasswordShowMessage(false);
   } else {
-    newPasswordShowMessage(true);
     window.setTimeout(() => {
       window.location.href = document.getElementById(
         NEW_PASSWORD_PAGE.form,
